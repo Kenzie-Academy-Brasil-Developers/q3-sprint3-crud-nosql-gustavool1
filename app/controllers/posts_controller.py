0e24831c9995cfb66c30275bb8f1783e9ed150c3
Gustavo <go8895806@gmail.com>
2022-01-24 12:35:31 -0300

    feat:fixing the generate_id func

--- app/controllers/posts_controller.py
+++ app/controllers/posts_controller.py
@@ -17,10 +17,15 @@ db = client[DATABASE]
 
 def generate_id():
     post_list = get_posts()
-    does_id_exist = db.get_collection(COLLECTION).find_one({"id":len(post_list) + 1})
-    if does_id_exist:
-        return len(post_list)+2
-    return len(post_list) + 1
+    id_list = []
+    for post in post_list:
+        id_list.append(int(post.get("id")))
+
+    if len(id_list) == 0:
+        return 1
+
+    return max(id_list) + 1
+
 
 def is_body_valid(body):
     post_keys=["author","content", "tags", "title"]
